544088b1a5ae0cc5eab89746c94e5f5ecdc89b78
Xavier Robinson <x.robinson5041@outlook.com>
2024-05-07 12:55:27 -0400

    loops down

---
+++ LoopScripts/for_loops.js
@@ -0,0 +1,11 @@
+"use strict"
+
+
+
+let i;
+let message = " I love Loops "
+for (i = 0; i < 7; i++) {
+
+ console.log(message);
+
+}
---
+++ LoopScripts/while_loops.js
@@ -0,0 +1,10 @@
+"use strict"
+
+
+let message = "I love loops"
+let i = 1;
+while (i < 8) {
+ 
+ console.log(message);
+ i++;
+}
